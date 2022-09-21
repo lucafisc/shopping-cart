c57a801477306151d122337f7d7fb7d92e5ff40e
lucafisc <lucafischer@fastmail.com>
2022-09-21 12:27:51 +0200

    Add page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import About from "./components/About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Nav.css";
 import favoritesIcon from "../images/icons/favorite-white.png";
 import shoppingIcon from "../images/icons/shopping-bag-white.png";
@@ -7,9 +8,15 @@ export default function Nav() {
       <div className="nav--logo">logo</div>
       <div className="nav--links">
         <ul>
-          <li>Home</li>
-          <li>Shop</li>
-          <li>About</li>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/shop">
+            <li>Shop</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
         </ul>
         <div className="nav--favorites">
           <img src={favoritesIcon} />
