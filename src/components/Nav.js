9e59d312fc091f9bf4248610fa6a0ea84c8318e0
lucafisc <lucafischer@fastmail.com>
2022-09-21 11:54:04 +0200

    Remove default boilerplate and add Navbar component

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import "./Nav.css";
+import favoritesIcon from "../images/icons/favorite-white.png";
+import shoppingIcon from "../images/icons/shopping-bag-white.png";
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <div className="nav--logo">logo</div>
+      <div className="nav--links">
+        <ul>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About</li>
+        </ul>
+        <div className="nav--favorites">
+          <img src={favoritesIcon} />
+        </div>
+        <div className="nav--shopping-cart">
+          <img src={shoppingIcon} />
+        </div>
+      </div>
+    </nav>
+  );
+}
