c71552507ace716bedcdbaa76af943671496df4c
lucafisc <lucafischer@fastmail.com>
2022-09-27 19:26:49 +0200

    Add about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,29 @@
 import "./About.css";
 
 export default function About() {
-  return <div>about our shop</div>;
+  return (
+    <div className="about">
+      <ul>
+        <li>this is a fake website built using React.</li>
+        <li>
+          data fetched from the{" "}
+          <a href="https://makeup-api.herokuapp.com/">Makeup AP</a>.
+        </li>
+        <li>
+          fonts used are{" "}
+          <a href="https://velvetyne.fr/fonts/picnic/">Picnic by Velvetyne</a>{" "}
+          and{" "}
+          <a href="https://fonts.google.com/specimen/Karla ">Karla by Google</a>
+          .
+        </li>
+        <li>
+          home image by{" "}
+          <a href="https://unsplash.com/photos/VItxz6u036U">
+            Curology on Unsplash
+          </a>
+          .
+        </li>
+      </ul>
+    </div>
+  );
 }
