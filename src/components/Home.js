7aaea903329b67c01b73526d8f0f60367fa4f0ff
lucafisc <lucafischer@fastmail.com>
2022-09-27 13:48:21 +0200

    Add home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,25 @@
 import "./Home.css";
-
+import brands from "../images/brands.svg";
+import cover from "../images/curology-VItxz6u036U-unsplash.jpg";
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="home">
+      <div className="slogan">
+        <h1 className="fade-in">shop products</h1>
+        <h1 className="fade-in">
+          from <span className="creators">creators</span>
+        </h1>
+        <h1 className="fade-in">
+          and <span className="brands">brands</span>
+        </h1>
+        <img src={brands} className="yellow-svg" />
+      </div>
+      <img src={cover} className="cover" />
+      <div className="shop-now">
+        <h1>
+          shop now <span className="arrows">{">>>"}</span>
+        </h1>
+      </div>
+    </div>
+  );
 }
