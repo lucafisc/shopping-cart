7c6187865378afe92d905d7d094825d91ccd05e8
lucafisc <lucafischer@fastmail.com>
2022-09-28 10:33:57 +0200

    Add basename prop to Router

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Nav
           shoppingCart={shoppingCart}
